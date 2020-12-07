a62e2b2e349d5dc4e08c721b3da5d2fa20a7253f
MartinDiMarco <tinchodimarco11@gmail.com>
2020-12-07 11:38:58 -0300

    Botones Agregados

--- Practico 2/js/scripts/main.js
+++ Practico 2/js/scripts/main.js
@@ -8,6 +8,10 @@ let createHeader = (claves) => {
       thEl.innerHTML = claves[i];
       trEl.appendChild(thEl);
     }
+    let thElim = document.createElement('th');
+    thElim.textContent = "Modificar";
+    thElim.classList.add('bg-info');
+    trEl.appendChild(thElim);
     theadEl.appendChild(trEl);
     tableEl.appendChild(theadEl);
   };
@@ -20,6 +24,21 @@ let createRow = (elemento) => {
       tdEl.innerHTML = elemento[key];
       trEl.appendChild(tdEl);
     }
+    let buttonElim = document.createElement('td');
+    let botonEliminar = document.createElement('button');
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.classList.add('btn-primary');
+    botonEliminar.classList.add('btn');
+    botonEliminar.classList.add('button-margin'),
+    buttonElim.appendChild(botonEliminar);
+    trEl.appendChild(buttonElim);
+    //otro boton
+    let botonModif = document.createElement('button');
+    botonModif.textContent = "Editar";
+    botonModif.classList.add('btn-primary');
+    botonModif.classList.add('btn');
+    buttonElim.appendChild(botonModif);
+
     return trEl;
   };
 
--- Practico 2/js/scripts/utilities.js
+++ Practico 2/js/scripts/utilities.js
@@ -2,63 +2,66 @@ let data = `{
     "paises": [
       {
         "País": "Suiza",
-        "Salario minimo": 4480,
+        "Salario minimo": "$4480",
         "Play Station 5": "3 días y medio",
         "Ultimo Iphone": "8 días",
         "Transporte público": "1244 viajes",
-        "Canasta basica": 1890,
-        "Editar": "Editar",
-        "Eliminar": "${btnDeleteEl}"
+        "Canasta basica": "$1890",
+        "Indice Libertad": "#4"
       },
       {
         "País": "Argentina",
-        "Salario minimo": 101,
+        "Salario minimo": "$101",
         "Play Station 5": "5 meses",
         "Ultimo Iphone": "1 año",
         "Transporte público": "937 viajes",
-        "Canasta basica": 112.5,
-        "Editar": "Editar",
-        "Eliminar": "eliminar"
+        "Canasta basica": "$112.5",
+        "Indice Libertad": "#156"
       },
       {
         "País": "Irlanda",
-        "Salario minimo": "1950",
+        "Salario minimo": "$1950",
         "Play Station 5": "7 días y medio",
         "Ultimo Iphone": "19 días",
         "Transporte público": "1142 viajes",
-        "Canasta basica": 709,
-        "Editar": "Editar",
-        "Eliminar": "eliminar"
+        "Canasta basica": "$709",
+        "Indice Libertad": "#9"
       },
       {
         "País": "Venezuela",
-        "Salario minimo": 1.84,
+        "Salario minimo": "$1.84",
         "Play Station 5": "23 años",
         "Ultimo Iphone": "56 años",
         "Transporte público": "27 viajes",
-        "Canasta basica": 229,
-        "Editar": "Editar",
-        "Eliminar": "eliminar"
+        "Canasta basica": "$229",
+        "Indice Libertad": "#179"
       },
       {
         "País": "Australia",
-        "Salario minimo": 2200,
+        "Salario minimo": "$2200",
         "Play Station 5": "5 días",
         "Ultimo Iphone": "14 días",
         "Transporte público": "1100 viajes",
-        "Canasta basica": 780,
-        "Editar": "Editar",
-        "Eliminar": "eliminar"
+        "Canasta basica": "$780",
+        "Indice Libertad": "#5"
       },
       {
         "País": "Chile",
-        "Salario minimo": 425,
+        "Salario minimo": "$425",
         "Play Station 5": "1 mes y medio",
         "Ultimo Iphone": "2 meses y medio",
         "Transporte público": "420 viajes",
-        "Canasta basica": 92,
-        "Editar": "Editar",
-        "Eliminar": "eliminar"
+        "Canasta basica": "$92",
+        "Indice Libertad": "#10"
+      },
+      {
+        "País": "Canadá",
+        "Salario minimo": "$1850",
+        "Play Station 5": "8 días y medio",
+        "Ultimo Iphone": "21 días",
+        "Transporte público": "1100 viajes",
+        "Canasta basica": "$690",
+        "Indice Libertad": "#7"
       }
     ]
   }
